4c231c1c37243575ae43312691198a67fceeb504
codelockinfo <codelockinfo@gmail.com>
2023-07-08 13:54:50 +0530

    step5

--- user/include.php
+++ user/include.php
@@ -9,5 +9,4 @@ if(isset($_GET['destroy'])){
 $store = (isset($_GET['store']) && $_GET['store'] != '') ? $_GET['store'] : "managedashboard.myshopify.com";
 $ologin = new Login($store);
 $functions = new Client_functions();
-echo "STEP";die;
 ?>
