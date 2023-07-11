3eebe2d2e3a01c045587afd62b4279d26223f2d9
codelockinfo <codelockinfo@gmail.com>
2023-07-11 18:13:15 +0530

    index header btn changes & footer add copy right text

--- user/dashboard.php
+++ user/dashboard.php
@@ -229,6 +229,10 @@ if (isset($_GET['store']) && $_GET['store'] != '') {
             </div>
         </div>  
     </div>
+    <center><div class="Polaris-FooterHelp__Content footermargin">
+                           <div class="Polaris-FooterHelp__Text">
+                                    ReWriter © 2021 - Developed by <a target="_blank" href="http://codelocksolutions.com/">Codelock Solutions</a>  team </div> 
+        </div></center>
 </body>
 </html> 
 <script>
