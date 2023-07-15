5c1f8e832183d8568fcabbddb1f838a68285af96
codelockinfo <codelockinfo@gmail.com>
2023-07-15 18:09:52 +0530

    Remove extra logs from uninstall webhook

--- webhook/app-uninstalled.php
+++ webhook/app-uninstalled.php
@@ -11,7 +11,6 @@ $cls_functions = new Client_functions($shop);
 
 function verify_webhook($data, $hmac_header, $cls_functions)
 {
-    generate_log('uninstall-webhook' , " verify webhook function calling ");
     $where_query = array(["", "status", "=", "1"]);
     $comeback= $cls_functions->select_result(CLS_TABLE_THIRDPARTY_APIKEY, '*',$where_query);
     $SHOPIFY_SECRET = (isset($comeback['data'][2]['thirdparty_apikey']) && $comeback['data'][2]['thirdparty_apikey'] !== '') ? $comeback['data'][2]['thirdparty_apikey'] : '';
@@ -21,25 +20,8 @@ function verify_webhook($data, $hmac_header, $cls_functions)
 
 $data = file_get_contents('php://input');
 $verified = verify_webhook($data, $hmac_header, $cls_functions);
-generate_log('uninstall-webhook' , json_encode($verified) . "     VERIFIED"); //check error.log to see the result
-generate_log('uninstall-webhook' , var_export($verified, true)); //check error.log to see the result
-
-// function verify_webhook($data, $hmac_header) {
-//     $calculated_hmac = base64_encode(hash_hmac('sha256',$data,SHOPIFY_APP_SECRET,true));
-//     generate_log('testingwebhook', $calculated_hmac . "   calculated_hmac");
-//     return hash_equals($hmac_header, $calculated_hmac);
-//     // return ($hmac_header == $calculated_hmac);
-// }
-// $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
-// generate_log('testingwebhook', $hmac_header . "  hmac_header ");
-// $datas = file_get_contents('php://input');
-// $verified = verify_webhook($datas, $hmac_header);
-//     generate_log('testingwebhook',var_export($verified, true)); //check error.log to see the result
-  
-
 
 if($verified == true){
-   generate_log('uninstall-webhook', json_encode($verified) . "  verified"); 
     if( $topic_header == "app/uninstalled" ) {
         $shop = $_SERVER['HTTP_X_SHOPIFY_SHOP_DOMAIN'];
         $fields = array(
