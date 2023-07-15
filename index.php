f8bd01c38112e77e696bde9b9a2461ee10b738b7
codelockinfo <codelockinfo@gmail.com>
2023-07-15 18:50:35 +0530

    Changes logs

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if(isset($_GET['shop'])){
 else {
     header('X-Frame-Options:SAMEORIGIN');
 }
-generate_log('URL_TRACKING', "STEP 1 START");
+
 if (DB_OBJECT == 'mysql') {
     include ABS_PATH . "/collection/mongo_mysql/mysql/common_function.php";
 } else {
@@ -16,6 +16,7 @@ if (DB_OBJECT == 'mysql') {
 require_once(ABS_PATH . '/cls_shopifyapps/config.php');
 require_once(ABS_PATH . '/cls_shopifyapps/cls_shopify.php');
 require_once(ABS_PATH . '/cls_shopifyapps/cls_shopify_call.php');
+
 $__webhook_arr = array(
     'app/uninstalled',
     'products/create',
@@ -25,9 +26,8 @@ $__webhook_arr = array(
     'collections/update',
     'collections/delete'
 );
-generate_log('URL_TRACKING', "STEP 1");
+
 if ($_GET['shop'] != "") {
-    generate_log('URL_TRACKING', "GET SHOP");
     $cls_functions = new common_function($_GET['shop']);
     $where_query = array(["", "status", "=", "1"]);
     $comeback= $cls_functions->select_result(CLS_TABLE_THIRDPARTY_APIKEY, '*',$where_query);
@@ -38,7 +38,6 @@ if ($_GET['shop'] != "") {
         die;
     }
     if (isset($_GET['code'])) {
-         generate_log('location', json_encode($_GET) . "GET SHOP");
         $shopifyClient = new ShopifyClient($_GET['shop'], "", $CLS_API_KEY, $SHOPIFY_SECRET);
         $password = $shopifyClient->getEntrypassword($_GET['code']);
         $shop = $_GET['shop'];
@@ -46,32 +45,24 @@ if ($_GET['shop'] != "") {
         $comeback_client = $cls_functions->select_result(TABLE_USER_SHOP, '*', $where_query, ['single' => true]);
    
         if ($comeback_client['status'] == 1) {
-            generate_log('URL_TRACKING', "GET status 1");
             $shop_row = $comeback_client['data'];
             header('Location: ' . SITE_CLIENT_URL . '?store=' . $shop);
         } else {
-            generate_log('URL_TRACKING', $password. "GET status 0");
             $shopuinfo = shopify_call($password, $shop, "/admin/".CLS_API_VERSIION."/shop.json", array(), 'GET');
-             generate_log('URL_TRACKING', json_encode($shopuinfo) ."shopuinfo");
-             generate_log('URL_TRACKING', json_encode($shopuinfo['response']) ."shopuinfo Response");
-             $shopuinfo = json_decode($shopuinfo['response']);
+            $shopuinfo = json_decode($shopuinfo['response']);
             
             $path = '/admin/api/2021-07/webhooks.json';
             $store_password = md5($SHOPIFY_SECRET . $password);
             $baseurl = "https://" . $CLS_API_KEY . ":" . $password . "@" . $shop . "/";
             $shopify_url = $baseurl . ltrim($path, '/');
             if (!empty($__webhook_arr)) {
-                
-            generate_log('webhook',json_encode($__webhook_arr) );
                 foreach ($__webhook_arr as $topic) {
                     $file_name = str_replace('/', '-', $topic) . '.php';
                     $params = '{"webhook": {"topic":"' . $topic . '",
                                "address":"https://codelocksolutions.in/cls-rewriter/webhook/' . $file_name . '",
                                 "format":"json"
 				}}';
-				
-                    $responce = $cls_functions->register_webhook($shopify_url, $params, $password);
-                    generate_log('webhook', json_encode($responce));
+                $responce = $cls_functions->register_webhook($shopify_url, $params, $password);
                 }
             }
             $asset = array("script_tag" =>
@@ -100,22 +91,14 @@ if ($_GET['shop'] != "") {
             );
           
             $result = $cls_functions->registerNewClientApi($store_information);
-            generate_log('URL_TRACKING', json_encode($result) , "   result");
-            generate_log('location', 'Location: ' . SITE_CLIENT_URL . '?store=' . $shop);
-            // header('Location: ' . SITE_CLIENT_URL . '?store=' . $shop);
-            
             header('Location: https://' . $shop . '/admin/apps/' . $CLS_API_KEY);
             exit;
         }
     } else {
-        
-        // generate_log('get ', json_encode($_GET) . "GET");
-        //   generate_log('post ', json_encode($_POST) . "POST");
         $shop = isset($_POST['shop']) ? $_POST['shop'] : $_GET['shop'];
         $where_query = array(["", "store_name", "=", "$shop"], ["AND", "status", "=", "1"]);
         $comeback = $cls_functions->select_result(TABLE_USER_SHOP, '*', $where_query, ['single' => true]);
         if ($comeback['status'] == 1) {
-            generate_log('Location: ' . SITE_CLIENT_URL . '?store=' . $shop    . "install - uninstall location");
             header('Location: ' . SITE_CLIENT_URL . '?store=' . $shop);
         } else {
             $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $CLS_API_KEY . "&scope=" . urlencode(SHOPIFY_SCOPE) . "&redirect_uri=" . urlencode(SITE_PATH);
@@ -128,8 +111,7 @@ else{
     generate_log('URL_TRACKING', "NOT GET SHOP");
     generate_log('URL_TRACKING', $_POST['shop']."POST DATA");
     generate_log('URL_TRACKING', $_GET['shop']."GET DATA");
-    //   header('Location: https://apps.shopify.com/ReWriter-Mega-Description');
-    //   exit;
-
+    header('Location: https://apps.shopify.com/ReWriter-Mega-Description');
+    exit;
 }
 ?>
