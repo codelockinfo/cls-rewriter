c04eaff61253c9b8196cc8ad384c114462ff407e
codelockinfo <codelockinfo@gmail.com>
2023-07-24 13:36:43 +0530

    email log add

--- user/index.php
+++ user/index.php
@@ -11,7 +11,11 @@ $subject="Rewriter App";
 $headers ="From:". $shopinfo->email ." \r\n";     
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
- mail ($to, $subject, $message, $headers);	
+generate_log('user_index' , $shopinfo->email  . " ... EMAIL");
+generate_log('user_index' , json_encode($message)  . " ... MESSSAGE");
+$responceEmail = mail ($to, $subject, $message, $headers);	
+generate_log('user_index' , json_encode($responceEmail)  . " ... EMAIL RESPONSE");
+
 include 'dashboard.php';
 die;
 ?>
