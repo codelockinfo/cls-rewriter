c51049789766496c8a98476a0b7496a3b1ec9c89
codelockinfo <codelockinfo@gmail.com>
2023-07-24 13:30:45 +0530

    thankyou mail  add

--- user/index.php
+++ user/index.php
@@ -1,8 +1,17 @@
-
 <?php
 require_once('include.php');
 include_once('cls_header.php');
 $common_function = new common_function();
+$functions = new Client_functions($store);
+$shopinfo = $functions->current_store_obj;
+$shopinfo = (object)$shopinfo;
+$message = file_get_contents('thankemail_template.php');
+$to = "codelockinfo@gmail.com";	
+$subject="Rewriter App"; 
+$headers ="From:". $shopinfo->email ." \r\n";     
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+ mail ($to, $subject, $message, $headers);	
 include 'dashboard.php';
 die;
 ?>
---
+++ user/thankemail_template.php
@@ -0,0 +1,38 @@
+<?php
+ob_start();
+include_once('cls_header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
+        <div class="mail-img" style="background-color: #fff;margin: auto;padding: 50px 100px;border-radius: 30px 30px 0px 0px;">
+            <img src="<?php echo CLS_SITE_URL; ?>/assets/images/thankemail_template/banner1.png" alt="" style="width: 75%;margin: 0 auto;display: flex;">
+            <div class="details" style="text-align: center;">
+                <h3 style="font-size: 38px;margin-bottom: 15px;color: rgb(25, 25, 25);">Hi John Doe, <br>Welcome to ReWriter!</h3>
+                <p style="font-size: 25px;">Thank you for Installation!</p>
+            </div>
+        </div>
+    </div>
+    <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
+        <div class="mobile" style="background-color: #fff;padding: 57px 57px 0px 57px;display: flex;align-items: center;justify-content: center;margin-top: 20px;border-radius: 30px 30px 0px 0px;">
+
+            <div class="mobile-text" style="width: 10%;text-align: center;margin-right: 2%;">
+                <img src="<?php echo CLS_SITE_URL; ?>/assets/images/thankemail_template/Logo.png" alt="" style="max-width: 100%;width: 100%;">
+            </div>
+            <div class="mobile-img">
+              <h2>ReWriter - Mega Description</h2>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-text" style="max-width: 650px;text-align: center;margin: 0 auto;color: rgb(85, 85, 85);padding-bottom: 15px;">
+            <p style="margin-bottom: 20px;padding: 0 10px;">If you have any questions, feel free message us at  codelockinfo@gmail.com. All right reserved. Update email preferences or unsubscribe.</p>
+        </div>
+        </footer>
+    </div>
+</body>
+</html>
