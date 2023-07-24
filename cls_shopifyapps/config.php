3263b58a334d4468e55b5f14123a7abb899512e6
codelockinfo <codelockinfo@gmail.com>
2023-07-24 09:55:27 +0530

    customer scope remove

--- cls_shopifyapps/config.php
+++ cls_shopifyapps/config.php
@@ -1,5 +1,5 @@
 <?php
-define("SHOPIFY_SCOPE", "read_content,write_content,read_products,write_products,read_script_tags,write_script_tags,read_customers,write_customers");
+define("SHOPIFY_SCOPE", "read_content,write_content,read_products,write_products,read_script_tags,write_script_tags");
 define("SITE_PATH","https://codelocksolutions.in/cls-rewriter");
 
 ?>
